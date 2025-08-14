aae9f36dbcf1f956b3b818db47621c799f801763
RAFAEL BORGES DA SILVA <rafaelsilva007@cs.unicid.edu.br>
2025-08-14 20:42:41 -0300

    teste

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("Ola mundo!\n"); /* isso é um comentario | essa parte do codigo printa "Hello World!" */
+	return 0;
+}
