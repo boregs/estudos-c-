b1261d60e0e90744afddd437b305fd47c8b86ed4
RAFAEL BORGES DA SILVA <rafaelsilva007@cs.unicid.edu.br>
2025-08-28 21:26:29 -0300

    Arquivos aula 3: While loops

--- aula_03/tempconverter/main.c
+++ aula_03/tempconverter/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//função para converter uma temperatura em Celsius para Farenheit
+float CelsiusToFarenheit(float celsius) {
+    return (celsius * 9.0/5.0) + 32.0;
+}
+
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+
 }
