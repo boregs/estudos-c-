41657b3b964bdccf7979afd504b6f1bfc1b0aeac
RAFAEL BORGES DA SILVA <rafaelsilva007@cs.unicid.edu.br>
2025-09-11 20:59:26 -0300

    Switch cases

---
+++ aula_05/switchcases/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /* O switch na linguagem C é uma estrutura de controle de fluxo
+    que executa blocos de código específicos com base no valor de uma variável.
+    É uma alternativa mais limpa e eficiente para cadeias de instruções if-else if-else.
+    */
+
+    int x = 3;
+
+    switch (x){
+    case 1:
+        printf("um");
+        break;
+    case 2:
+        printf("dois");
+        break;
+    case 3:
+        printf("tres");
+        break;
+
+    }
+}
