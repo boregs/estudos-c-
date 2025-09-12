f7de07d824eafa409f7fa8cec349eceef992dc93
RAFAEL BORGES DA SILVA <rafaelsilva007@cs.unicid.edu.br>
2025-09-11 21:50:03 -0300

    Arquivos de treinamento simples

---
+++ aula_05/PrimProg/BaseNum/BaseNum/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\n%d", 65);
+    printf("\n%d", 65);
+    printf("\n%d", 65);
+    printf("\n%d", 65);
+    system("PAUSE");
+    return 0;
+}
---
+++ aula_05/PrimProg/PrimProg/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("System Pause\n");
+    system("PAUSE");
+    return 0;
+}
+
