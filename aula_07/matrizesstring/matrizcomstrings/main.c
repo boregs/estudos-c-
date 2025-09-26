5b61521f026fdc0403e827759ff39fd7714c383f
RAFAEL BORGES DA SILVA <rafaelsilva007@cs.unicid.edu.br>
2025-09-25 21:40:55 -0300

    Aula 07 - Matrizes com numeros e strings

--- aula_07/matrizesstring/matrizcomstrings/main.c
+++ aula_07/matrizesstring/matrizcomstrings/main.c
@@ -8,9 +8,12 @@ int main()
     int size = 0;
 
     printf("\n S1 antes da alocacao = %s\n", s1);
-    strcpy(s1, "Luan Gameplays");
+    strcpy(s1, "Giannis Antetokoumnpo");
     printf("\n S1 depois da alocacao =  %s\n", s1);
 
     size = strlen(s1);
     printf("\nTamanho da string: %d", size);
+
+    s1[17] = '7';
+    printf("\ns1 = %s\n", s1);
 }
